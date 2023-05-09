e80da45802efd02a3be84fa92495b2c98e816511
user-user <user-user>
2023-05-09 17:16:33 +0500

    pz28

---
+++ pz_28/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace pz_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car plohoy = new Car();
+            Police horoshiy = new Police();
+
+            plohoy.opovesh += horoshiy.Opoveshenie;
+            plohoy.zader += horoshiy.Zaderzhanie;
+            plohoy.umer += horoshiy.Smert;
+
+            plohoy.Razgon();
+        }
+    }
+}
