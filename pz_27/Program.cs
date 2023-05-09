4f4ac8ff470af2a0706de1a16959554643b21dae
user-user <user-user>
2023-05-09 16:40:52 +0500

    pz_27

---
+++ pz_27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace pz_27
+{
+    internal class Program
+    {
+        struct TRAIN
+        {
+            public string NAZN;
+            public int NUMR;
+            public TimeOnly TIME;
+
+            TRAIN(string nazn, int numr, TimeOnly time)
+            {
+                NAZN = nazn;
+                NUMR = numr;
+                TIME = time;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
