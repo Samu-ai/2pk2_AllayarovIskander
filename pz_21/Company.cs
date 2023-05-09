1065e7d5b634c5e3f6e8b46436fa9f4a6897cf70
user-user <user-user>
2023-05-09 14:54:16 +0500

    pz21

---
+++ pz_21/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Company
+    {
+        public string Name { get; set; }
+        public int Okpo { get; set; }
+        public DateTime DateOfRegistry { get; set; }
+        public Company(string name, int okpo, DateTime date)
+        {
+            Name = name;
+            Okpo = okpo;
+            DateOfRegistry = date;
+        }
+
+        public void GetCompanyInfo()
+        {
+            Console.WriteLine($"Название - {Name}\nДата регистрации = {DateOfRegistry}\nОкпо - {Okpo}");
+        }
+    }
+}
--- pz_21/Program.cs
+++ pz_21/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Company twitter = new Company("Twitter", 12345678, DateTime.Now);
+            twitter.GetCompanyInfo();
         }
     }
 }
